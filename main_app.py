17610e50fedef486f488858d338b7a92196aa1cb
moiz8910 <83336512+moiz8910@users.noreply.github.com>
2024-06-14 19:36:22 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,45 +1,46 @@
-#Library imports
+# Library imports
 import numpy as np
 import streamlit as st
 import cv2
 from keras.models import load_model
 
-
-#Loading the Model
+# Loading the Model
 model = load_model('dog_breed.h5')
 
-#Name of Classes
-CLASS_NAMES = ['Scottish Deerhound','Bernese Mountain Dog','Maltese Dog']
+# Name of Classes
+CLASS_NAMES = ['Scottish Deerhound', 'Bernese Mountain Dog', 'Maltese Dog']
 
-#Setting Title of App
+# Setting Title of App
 st.title("Dog Breed Prediction")
 st.markdown("Upload an image of the dog")
 
-#Uploading the dog image
+# Uploading the dog image
 dog_image = st.file_uploader("Choose an image...", type="png")
 submit = st.button('Predict')
-#On predict button click
+
+# On predict button click
 if submit:
-
-
     if dog_image is not None:
+        try:
+            # Convert the file to an OpenCV image.
+            file_bytes = np.asarray(bytearray(dog_image.read()), dtype=np.uint8)
+            opencv_image = cv2.imdecode(file_bytes, 1)
 
-        # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(dog_image.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
+            # Displaying the image
+            st.image(opencv_image, channels="BGR")
 
+            # Resizing the image
+            opencv_image = cv2.resize(opencv_image, (224, 224))
 
+            # Convert image to 4 Dimension
+            opencv_image = np.expand_dims(opencv_image, axis=0)
 
-        # Displaying the image
-        st.image(opencv_image, channels="BGR")
-        #Resizing the image
-        opencv_image = cv2.resize(opencv_image, (224,224))
-        #Convert image to 4 Dimension
-        opencv_image.shape = (1,224,224,3)
-        #Make Prediction
-        Y_pred = model.predict(opencv_image)
+            # Make Prediction
+            Y_pred = model.predict(opencv_image)
+            predicted_class = CLASS_NAMES[np.argmax(Y_pred)]
 
-        st.title(str("The Dog Breed is "+CLASS_NAMES[np.argmax(Y_pred)]))
-
+            st.title(f"The Dog Breed is {predicted_class}")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
     else:
-        print ("The photo is not correct")
+        st.error("Please upload an image.")
