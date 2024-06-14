4e09cfba0396b72a1802ed611dfe31f279b52557
moiz8910 <83336512+moiz8910@users.noreply.github.com>
2024-06-14 06:41:07 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,50 +1,45 @@
-# Library imports
+#Library imports
 import numpy as np
 import streamlit as st
+import cv2
 from keras.models import load_model
-from PIL import Image
 
-# Loading the Model
+
+#Loading the Model
 model = load_model('dog_breed.h5')
 
-# Name of Classes
-CLASS_NAMES = ['Scottish Deerhound', 'Bernese Mountain Dog', 'Maltese Dog']
+#Name of Classes
+CLASS_NAMES = ['Scottish Deerhound','Bernese Mountain Dog','Maltese Dog']
 
-# Setting Title of App
+#Setting Title of App
 st.title("Dog Breed Prediction")
 st.markdown("Upload an image of the dog")
 
-# Uploading the dog image
+#Uploading the dog image
 dog_image = st.file_uploader("Choose an image...", type="png")
 submit = st.button('Predict')
-
-# On predict button click
+#On predict button click
 if submit:
+
+
     if dog_image is not None:
-        try:
-            # Convert the file to a PIL Image
-            pil_image = Image.open(dog_image)
-            # Resize the image
-            resized_image = pil_image.resize((224, 224))
-            # Convert the PIL Image to a NumPy array
-            image_array = np.array(resized_image)
-            # Convert image to RGB if it has 4 channels
-            if image_array.shape[2] == 4:
-                image_array = image_array[:, :, :3]
-            # Normalize the image
-            normalized_image = image_array / 255.0
-            # Expand dimensions to match the model's input shape
-            input_data = np.expand_dims(normalized_image, axis=0)
-            
-            # Make Prediction
-            Y_pred = model.predict(input_data)
-            breed = CLASS_NAMES[np.argmax(Y_pred)]
 
-            st.title(f"The Dog Breed is {breed}")
+        # Convert the file to an opencv image.
+        file_bytes = np.asarray(bytearray(dog_image.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
 
-        except Exception as e:
-            st.error(f"An unexpected error occurred: {str(e)}")
-            st.write(f"Unexpected error: {str(e)}")
+
+
+        # Displaying the image
+        st.image(opencv_image, channels="BGR")
+        #Resizing the image
+        opencv_image = cv2.resize(opencv_image, (224,224))
+        #Convert image to 4 Dimension
+        opencv_image.shape = (1,224,224,3)
+        #Make Prediction
+        Y_pred = model.predict(opencv_image)
+
+        st.title(str("The Dog Breed is "+CLASS_NAMES[np.argmax(Y_pred)]))
 
     else:
-        st.error("Please upload a valid image file.")
+        print ("The photo is not correct")
